2a143471a51ee0a5a6ca65f54841fd74f80bca93
stnava <stnava@fd2da0a4-d968-48df-9cb5-d8c496287cbf>
2010-06-19 00:56:28 +0000

    this version uses FMM to find a reasonable tri-partite loop around the extracted region ... guaranteed (i think) to work.
    git-svn-id: https://advants.svn.sourceforge.net/svnroot/advants@574 fd2da0a4-d968-48df-9cb5-d8c496287cbf

--- Temporary/itkManifoldIntegrationAlgorithm.cxx
+++ Temporary/itkManifoldIntegrationAlgorithm.cxx
@@ -596,7 +596,7 @@ void ManifoldIntegrationAlgorithm<TGraphSearchNode>::FindPath()
       m_CurrentNode=m_QS->m_Q.top();
       m_CurrentCost=m_CurrentNode->GetTotalCost();
       //    if ( m_CurrentCost / m_MaxCost > 0.5 ) 
-       this->ParameterizeBoundary( this->m_CurrentNode );  
+      //       this->ParameterizeBoundary( this->m_CurrentNode );  
       m_QS->m_Q.pop();
       if (!m_CurrentNode->GetDelivered()) {
 	m_QS->IncrementTimer();
@@ -614,7 +614,7 @@ void ManifoldIntegrationAlgorithm<TGraphSearchNode>::FindPath()
   //" num searched " << m_NumberSearched << " \n";
   
   //  std::cout << " Max Distance " << m_CurrentCost << std::endl;
-  // this->GetSearchBoundary(); 
+   this->GetSearchBoundary(); 
   
   return;
 
