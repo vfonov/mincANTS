c1d6ef370d035acd2a52f307b3e78696885d8081
stnava <stnava@fd2da0a4-d968-48df-9cb5-d8c496287cbf>
2011-06-01 19:12:42 +0000

    ENH: a test for DiReCT cortical thickness estimation based on ground truth data .
    git-svn-id: https://advants.svn.sourceforge.net/svnroot/advants@882 fd2da0a4-d968-48df-9cb5-d8c496287cbf

---
+++ Examples/Data/test_DiReCT.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+SEGOUT=test_image_seg.nii.gz
+GM=segger_prob_02.nii.gz 
+WM=segger_prob_03.nii.gz 
+GT=test_image_ground_truth.nii.gz
+THICKOUT=test_image_thickness.nii.gz 
+cp $SEGOUT temp.nii.gz 
+ImageMath 3 temp.nii.gz CorruptImage temp.nii.gz 0.05 0.1
+ThresholdImage 3 temp.nii.gz mask.nii.gz 0.1 9999 
+ImageMath 3 mask.nii.gz GetLargestComponent mask.nii.gz 
+Atropos -d 3 -a temp.nii.gz -i kmeans[3] -o [segger.nii.gz,segger_prob_%02d.nii.gz] -c [3,0] -m [0.0,1x1x1] -x mask.nii.gz  
+# ThresholdImage 3 $SEGOUT $WM 3 3 
+# SmoothImage 3  $WM 0.8  $WM 1
+# ThresholdImage 3 $SEGOUT $GM 2 2  
+# SmoothImage 3  $GM 0.8  $GM 1
+# KellySlater 3  test_image_seg.nii.gz test_image_wm.nii.gz test_image_gm.nii.gz   test_thick.nii.gz 0.1 45 10 0 1.
+
+rm direct_eval_log.txt 
+THPROG=KellyKapowski
+smoothing=1
+for r in 0.010 0.015 0.020 0.025 0.05 0.075 0.1 0.125 0.15 ; do 
+  thexe="$THPROG -d 3 -s ${SEGOUT} -w $WM -g $GM -o $THICKOUT -r $r --convergence [ 45 , 0 , 5 ] -t 10 -m $smoothing "
+  echo $thexe 
+  $thexe 
+  ImageMath 3 diff.nii.gz - $THICKOUT $GT
+  ImageMath 3 diff_slice.nii.gz ExtractSlice diff.nii.gz 30 
+  ImageMath 3 gt_slice.nii.gz ExtractSlice $GT 30 
+  ThresholdImage 2 gt_slice.nii.gz gt_slice.nii.gz 2 999 
+  MeasureMinMaxMean 2 diff_slice.nii.gz direct_eval_log.txt  1 gt_slice.nii.gz 
+done 
+rm $WM $GM temp.nii.gz mask.nii.gz segger*
+
