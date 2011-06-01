44aba771c727ada47840c7dcfec8b365fd522d2f
stnava <stnava@fd2da0a4-d968-48df-9cb5-d8c496287cbf>
2011-06-01 19:29:30 +0000

    ENH: a test for DiReCT cortical thickness estimation based on ground truth data .
    git-svn-id: https://advants.svn.sourceforge.net/svnroot/advants@883 fd2da0a4-d968-48df-9cb5-d8c496287cbf

--- Examples/Data/test_DiReCT.sh
+++ Examples/Data/test_DiReCT.sh
@@ -18,15 +18,19 @@ Atropos -d 3 -a temp.nii.gz -i kmeans[3] -o [segger.nii.gz,segger_prob_%02d.nii.
 rm direct_eval_log.txt 
 THPROG=KellyKapowski
 smoothing=1
-for r in 0.010 0.015 0.020 0.025 0.05 0.075 0.1 0.125 0.15 ; do 
+# for r in 0.010 0.015 0.020 0.025 0.05 0.075 0.1 0.125 0.15 ; do 
+rct=10
+for r in 0.15 0.175 0.20 0.225 ; do
   thexe="$THPROG -d 3 -s ${SEGOUT} -w $WM -g $GM -o $THICKOUT -r $r --convergence [ 45 , 0 , 5 ] -t 10 -m $smoothing "
   echo $thexe 
   $thexe 
   ImageMath 3 diff.nii.gz - $THICKOUT $GT
-  ImageMath 3 diff_slice.nii.gz ExtractSlice diff.nii.gz 30 
+  ImageMath 3 diff_${rct}_slice.nii.gz ExtractSlice diff.nii.gz 30 
   ImageMath 3 gt_slice.nii.gz ExtractSlice $GT 30 
   ThresholdImage 2 gt_slice.nii.gz gt_slice.nii.gz 2 999 
-  MeasureMinMaxMean 2 diff_slice.nii.gz direct_eval_log.txt  1 gt_slice.nii.gz 
+  MeasureMinMaxMean 2 diff_${rct}_slice.nii.gz direct_eval_log.txt  1 gt_slice.nii.gz 
+  MeasureMinMaxMean 2 diff_${rct}_slice.nii.gz direct_eval_log.txt  0 gt_slice.nii.gz
+  let rct=$rct+1
 done 
 rm $WM $GM temp.nii.gz mask.nii.gz segger*
 
