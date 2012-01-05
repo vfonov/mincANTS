0a7df5ad75f492fb8ae4aaad2ff00deb2dd63e9b
ntustison <ntustison@fd2da0a4-d968-48df-9cb5-d8c496287cbf>
2012-01-05 12:42:32 +0000

    BUG: number of iterations was behind a level
    git-svn-id: https://advants.svn.sourceforge.net/svnroot/advants@1069 fd2da0a4-d968-48df-9cb5-d8c496287cbf

--- Examples/antsRegistration.cxx
+++ Examples/antsRegistration.cxx
@@ -108,7 +108,10 @@ public:
 
     GradientDescentOptimizerType * optimizer = reinterpret_cast<GradientDescentOptimizerType *>(
       const_cast<typename TFilter::OptimizerType *>( filter->GetOptimizer() ) );
-    optimizer->SetNumberOfIterations( this->m_NumberOfIterations[currentLevel] );
+    if( currentLevel + 1 < this->m_NumberOfIterations.size() )
+      {
+      optimizer->SetNumberOfIterations( this->m_NumberOfIterations[currentLevel + 1] );
+      }
     }
 
   void SetNumberOfIterations( std::vector<unsigned int> iterations )
